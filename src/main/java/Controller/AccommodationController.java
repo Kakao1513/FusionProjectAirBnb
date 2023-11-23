a6fc857d08c5fd0f8c315ec14bbcd99676cfce68
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2023-11-24 01:05:36 +0900

    Revert "IOC Container 적용, 숙소필터링 기능 네트워킹 적용"
    This reverts commit 5ba3b633af256faf60a0135236dc5c98623d31eb.

--- src/main/java/Controller/AccommodationController.java
+++ src/main/java/Controller/AccommodationController.java
@@ -1,27 +1,24 @@
 package Controller;
 
+import network.Protocol.Enums.Method;
+import network.Protocol.Enums.RoleType;
+import network.Protocol.Request;
+import network.Protocol.Response;
 import persistence.dto.AccommodationDTO;
-import persistence.dto.ReservationDTO;
-import persistence.dto.UserDTO;
-import persistence.dto.RatePolicyDTO;
 import service.AccommodationService;
-import view.AccommodationView;
 
-import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class AccommodationController {
-    private UserDTO currentUser;
-    private AccommodationView accomView;
-    private AccommodationService accomService;
+public class AccommodationController implements MethodController {
+	private final AccommodationService accomService;
 
-    public AccommodationController(AccommodationService accomService){
-        this.accomService = accomService;
-    }
+	public AccommodationController(AccommodationService accomService) {
+		this.accomService = accomService;
+	}
 
-    public void accomMenu(){
+	/*
+	public void accomMenu() {
         int order = accomView.displayAccomMenu();
         switch (order) {
             case 1 -> accomListMenu();
@@ -30,87 +27,193 @@ public class AccommodationController {
             case 4 -> setAccomDiscount();
             default -> System.out.println("잘못 입력 하셨습니다.");
         }
-    }
+	}
+*/
 
-    public void accomListMenu(){
-        List<AccommodationDTO> curAccomList = accomService.selectAccom("승인됨");
-        while(true){
-            accomView.displayAccomList(curAccomList);
-            int order = accomView.getOrder();
-            switch (order) {
-                case 1 -> getAccomInfo();
-                case 2 -> curAccomList = setSearchFilters();
-                default -> {
-                    System.out.println("<<종료>>");
-                    return;
-                }
-            }
-        }
-    }
+	public Response selectAccomList() {
+		Response response = new Response();
+		response.setIsSuccess(true);
 
-    public void getAccomInfo(){
-        int accomID = accomView.getAccomNumberFromUser();
-        AccommodationDTO curAccom = accomService.getAccom(accomID);
-        if (curAccom != null){
-            accomView.displayAccomInfo(curAccom, accomService.getRate(curAccom));
-            accomView.displayAmenity(accomService.getAmenityList(curAccom));
-            accomView.displayReviews(accomService.getReviews(curAccom));
-            LocalDate date = accomView.getReservationDate();
-            List<ReservationDTO> reservationDTOS = accomService.getReservationList(curAccom, date);
-            accomView.displayReservationCalendar(date, curAccom.getCapacity(), reservationDTOS);
-        }
-    }
+		List<AccommodationDTO> curAccomList = accomService.selectAccom("승인됨");
+		response.setPayload(curAccomList);
+		return response;
+	}
 
-    public List<AccommodationDTO> setSearchFilters(){
-        Map<String, Object> filters = new HashMap<>();
-        filters.put("status", "승인됨");
-        while (true) {
-            int order = accomView.displayFilterList();
-            switch (order) {
-                case 1 -> filters.put("accomName", accomView.getAccomNameFromUser());
-                case 2 -> filters.put("period", accomView.getPeriodFromUser());
-                case 3 -> filters.put("capacity", accomView.getCapacityFromUser());
-                case 4 -> filters.put("accomType", accomView.getAccomTypeFromUser());
-                default -> {
-                    return accomService.selectAccom(filters);
-                }
-            }
-        }
-    }
+/*	public void getAccomInfo() {
+		int accomID = accomView.getAccomNumberFromUser();
+		AccommodationDTO curAccom = accomService.getAccom(accomID);
+		accomView.displayAccomInfo(curAccom, accomService.getRate(accomID));
+		accomView.displayAmenity(accomService.getAmenityList(accomID));
+		accomView.displayReviews(accomService.getReviews(accomID));
+		LocalDate date = accomView.getReservationDate();
+		List<ReservationDTO> reservationDTOS = accomService.getReservationList(accomID, date);
+		accomView.displayReservationCalendar(date, curAccom.getCapacity(), reservationDTOS);
+	}*/
 
-    public void insertAccom(){
-        accomService.insertAccom(accomView.getAccomInfoFromUser());
-    }
+/*	public List<AccommodationDTO> setSearchFilters() {
+		Map<String, Object> filters = new HashMap<>(); //Request의 Payload에 담겨서 온다.
+		filters.put("status", "승인됨");
+		while (true) {
+			int order = accomView.displayFilterList(); // Client로 가야됨.
+			switch (order) {
+				case 1 -> filters.put("accomName", accomView.getAccomNameFromUser());
+				case 2 -> filters.put("period", accomView.getPeriodFromUser());
+				case 3 -> filters.put("capacity", accomView.getCapacityFromUser());
+				case 4 -> filters.put("accomType", accomView.getAccomTypeFromUser());
+				default -> {
+					return accomService.selectAccom(filters);
+				}
+			}
+		}
+	}
 
-    private void setAccomPolicy() {
-        List<AccommodationDTO> curAccomList = accomService.selectAccom("승인됨");
-        while(true){
-            accomView.displayAccomList(curAccomList);
-            accomView.Return();
-            int order = accomView.getAccomNumberFromUser();
-            if (order == 0) {
-                accomMenu();
-                return;
-            } else {
-              //TODO:  accomService.setAccomPolicy(accomView.getRatePolicyFromUser(order));
-            }
-        }
-    }
+	public void insertAccom() {
+		accomService.insertAccom(accomView.getAccomInfoFromUser());
+	}*/
+/*
+	private void setAccomPolicy() {
+		List<AccommodationDTO> curAccomList = accomService.selectAccom("승인됨");
+		while (true) {
+			accomView.displayAccomList(curAccomList);
+			accomView.Return();
+			int order = accomView.getAccomNumberFromUser();
+			if (order == 0) {
+				accomMenu();
+				return;
+			} else {
+				//TODO:  accomService.setAccomPolicy(accomView.getRatePolicyFromUser(order));
+			}
+		}
+	}*/
+/*
 
-    public void setAccomDiscount() {
-        List<AccommodationDTO> curAccomList = accomService.selectAccom("승인됨");
-        while(true){
-            accomView.displayAccomList(curAccomList);
-            accomView.Return();
-            int order = accomView.getDailyOrDiscount();
-            switch (order) {
-                case 1 -> getAccomInfo();
-                case 2 -> curAccomList = setSearchFilters();
-                default -> {
-                    System.out.println("<<종료>>");
-                    return;
-                }
-            }
-        }
-    }
+	public void setAccomDiscount() {
+		List<AccommodationDTO> curAccomList = accomService.selectAccom("승인됨");
+		while (true) {
+			accomView.displayAccomList(curAccomList);
+			accomView.Return();
+			int order = accomView.getDailyOrDiscount();
+			switch (order) {
+				case 1 -> getAccomInfo();
+				case 2 -> curAccomList = setSearchFilters();
+				default -> {
+					System.out.println("<<종료>>");
+					return;
+				}
+			}
+		}
+	}
+*/
+
+	@Override
+	public Response handle(Request req) {
+		Method method = req.getMethod();
+		Response res = null;
+		switch (method) {
+			case GET -> {
+				res = getHandle(req);
+			}
+			case PUT -> {
+				res = putHandle(req);
+			}
+			case POST -> {
+				res = postHandle(req);
+			}
+			case DELETE -> {
+				res = deleteHandle(req);
+			}
+		}
+		return res;
+	}
+
+	@Override
+	public Response getHandle(Request req) {
+		RoleType roleType = req.getRoleType();
+		Response res = null;
+		switch (roleType) {
+			case COMMON -> {
+
+			}
+			case ADMIN -> {
+			}
+			case HOST -> {
+			}
+			case GUEST -> {
+				res = selectAccomList();
+			}
+		}
+		return res;
+
+	}
+
+	@Override
+	public Response putHandle(Request req) {
+		RoleType roleType = req.getRoleType();
+		Response res = null;
+		switch (roleType) {
+			case COMMON -> {
+
+			}
+			case ADMIN -> {
+
+			}
+			case HOST -> {
+
+			}
+			case GUEST -> { //숙소 필터링
+				res = accomFiltering(req);
+			}
+		}
+		return res;
+	}
+
+
+	@Override
+	public Response postHandle(Request req) {
+		RoleType roleType = req.getRoleType();
+		Response res = null;
+		switch (roleType) {
+			case COMMON -> {
+			}
+			case ADMIN -> {
+			}
+			case HOST -> {
+			}
+			case GUEST -> {
+			}
+
+		}
+
+		return res;
+	}
+
+	@Override
+	public Response deleteHandle(Request req) {
+		RoleType roleType = req.getRoleType();
+		Response res = null;
+		switch (roleType) {
+			case COMMON -> {
+			}
+			case ADMIN -> {
+			}
+			case HOST -> {
+			}
+			case GUEST -> {
+			}
+		}
+
+		return res;
+	}
+
+	public Response accomFiltering(Request req) {
+		Map<String, Object> filters = (Map<String, Object>) req.getPayload();
+		filters.put("status", "승인됨");
+		List<AccommodationDTO> acList = accomService.selectAccom(filters);
+		Response rs = new Response();
+		rs.setIsSuccess(true);
+		rs.setPayload(acList);
+		return rs;
+	}
 }
+
+
