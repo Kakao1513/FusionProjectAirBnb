aa87639a46788f57aeea461f4c35d25493638c60
alalalek <alalalek@naver.com>
2023-11-29 22:30:20 +0900

    UPDATE 숙소 예약 버그 수정
    숙소 예약 시 취소된 숙소도 고려하는 문제 해결

--- src/main/java/service/ReservationService.java
+++ src/main/java/service/ReservationService.java
@@ -163,7 +163,8 @@ public class ReservationService {
 
 
     public List<AccommodationDTO> getFilteredAccomList(Map<String, Object> filters){
-        filters.put("capacity", (int)filters.get("headcount"));
+        filters.put("capacity", filters.get("headcount"));
+        filters.put("notCancel", true);
         List<AccommodationDTO> accomDTOS = accomDAO.selectAccom(filters);
         List<ReservationDTO> reservationDTOS = reservationDAO.getReservations(filters);
         List<AccommodationDTO> filteredAccomList = new ArrayList<>();
